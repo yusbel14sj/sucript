ed2df33d8c97d15fb5c237a1f8d8d2b28f568ea6
yusbel <yusbel14sj@gmail.com>
2023-11-21 22:50:27 -0500

    java scritp

--- onclick.js
+++ onclick.js
@@ -1,17 +1,20 @@
 const email = document.getElementById("email");
-const label = document.getElementById("label")
+const label = document.getElementById("label");
 const success = "success.html";
+const regex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 
-function errorEmail(){
-    if(email.value=="email"){
-        window.location.href = success;
-
-    }else{
-        emailIncorect()
-    }
+function validarCorreo(){
+    if(!regex.test(email.value)){
+        emailIncorect();
+        return false;
+    } ;
+    nextPage();
+    return true
 }
 function emailIncorect(){
-    console.log(label);
     email.classList.add("email-error");
     label.classList.add("input-incorrect");
+}
+function nextPage(){
+    window.location.href = "success.html"
 }
